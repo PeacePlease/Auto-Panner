d0a006ce3953041b9311bc6bf3ec6493e32a4957
Peace_please <j.hoggett@live.co.uk>
2018-01-30 21:22:54 +0000

    Added logging and potential logarithm function

--- Python/main.py
+++ Python/main.py
@@ -1,31 +1,57 @@
 from scipy.io import wavfile as wav
 from scipy.fftpack import fft, ifft, fftfreq
+
 import numpy as np
 from bokeh.plotting import figure, show
+from math import log10
+
+import time
+from inspect import currentframe, getframeinfo
+from re import split as rsplit
+
+####################################################################################################
 
 plot_graphs = False
 rate = 0
 
-def ramp(freqs, startf, endf):
+def main():
 
-    print "\n\n==========================\n Generating ramp function \n==========================\n"
-    
-    ramp_l = np.zeros(len(freqs))
-    ramp_r = np.zeros(len(freqs))
+    start_time = time.time()
+    global start_time
 
-    for i in range(0, len(freqs)):
-        if   abs(freqs[i]) < startf:
-            ramp_l[i] = 1
-        elif abs(freqs[i]) > endf:
-            ramp_r[i] = 1
-        else:
-            ramp_r[i] = float(abs(freqs[i])-startf)/(endf-startf)
-            ramp_l[i] = 1-ramp_r[i]
+    log('Starting', currentframe())
     
-    plot(freqs, ramp_l, 'Ramp (L)', 'frequency', 'power')
-    plot(freqs, ramp_r, 'Ramp (R)', 'frequency', 'power')
-        
-    return ramp_l, ramp_r
+#Parameters
+    inpath     = '../InputSamples/16-bit/'
+    outpath    = '../OutputSamples/'
+    infile     = 'LeOnde.wav'
+    outfile    = infile
+    startrampf = 50
+    endrampf   = 1000
+    
+#Get FFT
+    freqs, fft_out = getFFT(inpath + infile)
+
+#Apply pan effect (mono in, stereo out)
+    process_out_l, process_out_r = pan(freqs, fft_out, startrampf, endrampf)
+
+#IFFT and write result
+    write_wav(process_out_l, process_out_r, outpath + outfile)
+
+####################################################################################################
+
+
+
+def log(text, currframe, level='INFO'):
+    global start_time
+    frameinfo = getframeinfo(currframe)
+    print(str(time.time() - start_time) + "\t| " + rsplit('[\\\/]', frameinfo.filename)[-1] + "\t| " +  str(frameinfo.lineno) + "\t| " + level + "\t| " + text)
+
+def wall(text, currframe, level='INFO'):
+    log('', currframe)
+    log('==============================', currframe, level)
+    log(text, currframe, level)
+    log('==============================', currframe, level)
 
 def plot(x, y, title, x_label, y_label, force=False):
     global plot_graphs
@@ -34,32 +60,40 @@ def plot(x, y, title, x_label, y_label, force=False):
         p.line(x, y, legend=title, line_width=1)
         show(p)
 
+def logiter(x):
+    return log10(abs(x)+1)
+def oneminus(x):
+    return 1-x
+
+####################################################################################################
+
 
 def getFFT(wav_path):
 
-    print "\n==========================\n Getting FFT of audio \n==========================\n"
+    wall('Getting FFT of audio', currentframe())
 
     global rate
     rate, data = wav.read(wav_path)
 
     if len(data) > 10000000:
-        print("WARNING: Cutting short for now")
+        log('Cutting short for now', currentframe(), 'WARN')
         data = data[:10000000]
     
     #For comparison with final product
-    print ("Shape of data: " + str(data.shape))
-    print ("Data type: " + str(data.dtype))
+    log("Shape of data: " + str(data.shape), currentframe())
+    log("Data type: " + str(data.dtype), currentframe())
     
     #Check if Mono, discard right side if not
     if   len(data.shape) >  2:
-        print "Error, more than 2 channels"
+        log('More than 2 channels', currentframe(), 'ERROR')
     elif len(data.shape) == 2:
-        print "WARNING: sample is stero - using left side only..."
+        log('Sample is stero - using left side only...', currentframe(), 'WARN')
         data = data[:, 0]
     elif len(data.shape) == 1:
-        print "Sample is mono, continuing"
+        log("Sample is mono, continuing", currentframe())
     else:
-        print "ERROR: Something's gone horribly wrong - stereo check"
+        log("Something's gone horribly wrong - stereo check", currentframe(), 'CRIT')
+        sys.exit(1)
     
     x = range(0, len(data))
     plot(x, data, 'Original audio signal', 'time', 'signal')
@@ -71,20 +105,66 @@ def getFFT(wav_path):
         
     return freqs, fft_out
 
-def pan(freqs, fft_out):
+def write_wav(left, right, path):
+    wall('Run IFFT and write audio out', currentframe())
+    final_out = np.vstack(( np.real(ifft(left)).astype('int16'), np.real(ifft(right)).astype('int16') )).transpose()
+    log("Shape of data: " + str(final_out.shape) + ", data type: " + str(final_out.dtype), currentframe())
+    wav.write(path, rate, final_out)
+    log("Written file to disk. Enjoy!", currentframe())
 
-    print "\n==========================\n Applying pan effect \n==========================\n"
+
+def ramp(freqs, startf, endf):
+
+    wall('Generating ramp function', currentframe())
+    
+    ramp_l = np.zeros(len(freqs))
+    ramp_r = np.zeros(len(freqs))
+
+    for i in range(0, len(freqs)):
+        if i == len(freqs)/2 and 0 == len(freqs)%2:
+            log('Halfway there...', currentframe())
+        
+        if   abs(freqs[i]) < startf:
+            ramp_l[i] = 1
+        elif abs(freqs[i]) > endf:
+            ramp_r[i] = 1
+        else:
+            ramp_r[i] = float(abs(freqs[i])-startf)/(endf-startf)
+            ramp_l[i] = 1-ramp_r[i]
+
+    log("There!", currentframe())
+    
+    plot(freqs, ramp_l, 'Ramp (L)', 'frequency', 'power')
+    plot(freqs, ramp_r, 'Ramp (R)', 'frequency', 'power')
+        
+    return ramp_l, ramp_r
+
+
+def logfunc(freqs, stretch):
+    logl = map(logiter, freqs)
+    logr = map(oneminus, logl)
+    log(str(freqs), currentframe())
+    log(str(logl), currentframe())
+    plot(freqs, logl, 'Log (L)', 'frequency', 'power')
+    return logl, logr
+
+def pan(freqs, fft_out, startrampf, endrampf):
+
+    wall('Applying pan effect', currentframe())
 
     #Find peak power
-    peak = np.real(np.amax(fft_out))
-    print("Max value: " + str(peak))
+    peaki = np.argmax(fft_out)
+    
+    
+    log("Peak is at " + str(round(freqs[peaki]/1000, 4)) + " kHz", currentframe())
     
     #Get number of frequency samples
-    print("Frequency samples: " + str(len(fft_out)))
-    print("Frequency range:   " + str(min(freqs)/1000) + "-" + str(max(freqs)/1000) + " kHz")
+    log("Frequency samples: " + str(len(fft_out)), currentframe())
+    log("Frequency range:   " + str(round(max(freqs)/1000, 4)) + " kHz", currentframe())
     
     #Get ramp functions
-    ramp_l, ramp_r = ramp(freqs, 50, 1000)
+    ramp_l, ramp_r = ramp(freqs, startrampf, endrampf)
+    #log_l, log_r   = logfunc(freqs, 1)
     
     process_out_l = fft_out * ramp_l
     process_out_r = fft_out * ramp_r
@@ -94,25 +174,8 @@ def pan(freqs, fft_out):
         
     return process_out_l, process_out_r
 
-
-def write_wav(left, right, path):
-    print "\n==========================\n Run IFFT and write audio out \n==========================\n"
-    final_out = np.vstack(( np.real(ifft(left)).astype('int16'), np.real(ifft(right)).astype('int16') )).transpose()
-    print ("Shape of data: " + str(final_out.shape) + ", data type: " + str(final_out.dtype))
-    wav.write(path, rate, final_out)
     
 ####################################################################################################
-
-def main():
-
-#Get FFT
-    freqs, fft_out = getFFT('../InputSamples/16-bit/LeOnde.wav')
-
-#Apply pan effect (mono in, stereo out)
-    process_out_l, process_out_r = pan(freqs, fft_out)
-
-#IFFT and write result
-    write_wav(process_out_l, process_out_r, '../OutputSamples/LeOnde.wav')
 
 if __name__ == "__main__":
     main()
